5c6c1c0cdb24e1a7abb9cee8a93fc9c17a51699f
Surgeon <Surgeon1989@gmail.com>
2014-12-11 18:57:14 +0300

    add breadcrumbs

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,9 @@
 module PagesHelper
+  def get_bread_crumbs(page)
+    if page.parent
+      "#{link_to('Главная', root_path)}  #{link_to page.parent.name, pages_full_path(page.parent.permalink, page.parent.permalink)}  #{page.name}".html_safe
+    else
+      "#{link_to('Главная', root_path)}  #{page.name}".html_safe
+    end
+  end
 end
